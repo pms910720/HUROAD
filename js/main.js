097205143d9c66c4f28d1c2f456e8034350e790c
pms910720 <pms910720@naver.com>
2022-09-25 22:40:28 +0900

    20220925

--- js/main.js
+++ js/main.js
@@ -18,55 +18,20 @@
 //   searchInputEl.setAttribute('placeholder','');
 // });
 
-function checkTouchable() {
-  document.body.dataset.touchable = !!window.ontouchstart;
-}
-
-checkTouchable();
-
 /**
  * 페이지 스크롤에 따른 요소 제어
  */
 // 페이지 스크롤에 영향을 받는 요소들을 검색!
-const badgeEl = document.querySelector('header .badges')
+const filter = "win16|win32|win64|mac|macintel";
 const toTopEl = document.querySelector('#to-top')
 
 // 페이지에 스크롤 이벤트를 추가!
 // 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
-if(document.body.dataset.touchable == "true"){
-  alert("why");
-  window.addEventListener('scroll', _.throttle(function () {
-    // 페이지 스크롤 위치가 500px이 넘으면.
-    if (window.scrollY > 500) {
-      // Badge 요소 숨기기!
-      gsap.to(badgeEl, .6, {
-        opacity: 0,
-        display: 'none'
-      })
-      // 상단으로 스크롤 버튼 보이기!
-      gsap.to(toTopEl, .2, {
-        x: 0
-      })
+if(filter.indexOf( navigator.platform.toLowerCase() ) < 0){
 
-    // 페이지 스크롤 위치가 500px이 넘지 않으면.
-    } else {
-      // Badge 요소 보이기!
-      gsap.to(badgeEl, .6, {
-        opacity: 1,
-        display: 'block'
-      })
-      // 상단으로 스크롤 버튼 숨기기!
-      gsap.to(toTopEl, .2, {
-        x: 100
-      })
-    }
-  }, 300))
-}else{
-      // Badge 요소 숨기기!
-      gsap.to(badgeEl, 0, {
-        opacity: 0,
-        display: 'none'
-      })
+  document.querySelector('.succulent').hide()
+  document.querySelector('.cat').hide()
+ 
 }
 
 // 상단으로 스크롤 버튼을 클릭하면,
